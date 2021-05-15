761f6761bd4ed4c190b54de564d8fdcf43df9562
arturkaldma <arturkaldma@gmail.com>
2021-05-15 10:19:11 +0300

    Initial commit

---
+++ imageFromWiki.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import wikipedia
+import requests
+
+Builder.load_file('frontendForWiki.kv')
+
+class FirstScreen(Screen):
+
+    def get_image_link(self):
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        # Download the image
+        req = requests.get(self.get_image_link())
+        imagepath = 'images/image.jpg'
+        with open(imagepath, 'wb') as file:
+            file.write(req.content)
+        return imagepath
+
+    def set_image(self):
+        # Set the image in the Image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
---
+++ main.py
@@ -0,0 +1,72 @@
+import webbrowser
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
+import time
+
+from filesharer import FileSharer
+
+Builder.load_file('frontend.kv')
+
+class CameraScreen(Screen):
+
+    def start(self):
+        """ Starts camera and changes Butxon text """
+        self.ids.camera.opacity = 1
+        self.ids.camera.play = True
+        self.ids.btncam.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+    def stop(self):
+        """ Stop camera and changes Button text """
+        self.ids.camera.opacity = 0
+        self.ids.camera.play = False
+        self.ids.btncam.text = "Start Camera"
+        self.ids.camera.texture = None
+
+    def capture(self):
+        """ Creates a filename with the current time and captures
+         and saves a photo image under that filename """
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        self.filepath = f"images/{current_time}.png"
+        self.ids.camera.export_to_png(self.filepath)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = self.filepath
+
+class ImageScreen(Screen):
+
+    link_message = "Create a Link First"
+
+    def create_link(self):
+        """ Accesses the photo filepath, uploads it to the web,
+        and inserts the link in the label widget """
+        file_path = self.ids.img.source
+        filesharer = FileSharer(filepath = file_path)
+        self.url = filesharer.share()
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        """ Copy link to the clipboard available for pasting """
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        """ Open link with default browser """
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
